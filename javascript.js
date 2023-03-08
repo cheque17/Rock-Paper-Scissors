6cf61f0d02a14e4cd8fbc85262022a39015a041b
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-03-08 13:46:07 -0600

    Edit index.html and javascript.js

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+    let computerChoiceRock = "rock";
+    let computerChoicePaper = "paper";
+    let computerChoiceScissors = "scissors";
+
+    let decisionMaker = Math.floor(Math.random() * 100);
+
+    if (decisionMaker >= 0 && decisionMaker <= 33) {
+        return computerChoiceRock;
+    } else if (decisionMaker >= 34 && decisionMaker <= 66) {
+        return computerChoicePaper;
+    } else {
+        return computerChoiceScissors;
+    }
+
+
+}
